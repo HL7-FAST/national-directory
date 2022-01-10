fd59db1236b263d6241467c038f4bf0317187f8d
Abigail Watson <awatson@mitre.org>
2022-01-09 22:17:19 -0600

    sync

--- client/ServerConfigurationPage.jsx
+++ client/ServerConfigurationPage.jsx
@@ -94,149 +94,149 @@ function ServerConfigurationPage(props){
     })
   }, [])
 
-  const formik = useFormik({
-    initialValues: {
-      iss: 'http://example.com/my-b2b-app',
-      sub: 'http://example.com/my-b2b-app',
-      aud: 'https://oauth.example.net/register',
-      exp: '1597186041',
-      iat: '1597186041',
-      jti: 'random-value-109a3bd72',
-      client_name: 'Acme B2B App',
-      redirect_uris: '',
-      contacts: "mailto:b2b-operations@example.com",
-      logo_uri: '',
-      grant_types: "client_credentials",
-      response_types: "",
-      token_endpoint_auth_method: "private_key_jwt",
-      scope: 'system/Patient.read system/Procedure.read'
-    },
-    validate: function(values){
-      const errors = {};
+  // const formik = useFormik({
+  //   initialValues: {
+  //     iss: 'http://example.com/my-b2b-app',
+  //     sub: 'http://example.com/my-b2b-app',
+  //     aud: 'https://oauth.example.net/register',
+  //     exp: '1597186041',
+  //     iat: '1597186041',
+  //     jti: 'random-value-109a3bd72',
+  //     client_name: 'Acme B2B App',
+  //     redirect_uris: '',
+  //     contacts: "mailto:b2b-operations@example.com",
+  //     logo_uri: '',
+  //     grant_types: "client_credentials",
+  //     response_types: "",
+  //     token_endpoint_auth_method: "private_key_jwt",
+  //     scope: 'system/Patient.read system/Procedure.read'
+  //   },
+  //   validate: function(values){
+  //     const errors = {};
       
-      if (!values.iss) {
-        errors.iss = 'Required';
-      }
-      if (!values.sub) {
-        errors.sub = 'Required';
-      }
-      if (!values.aud) {
-        errors.aud = 'Required';
-      }
-      if (!values.exp) {
-        errors.exp = 'Required';
-      }
-      if (!values.iat) {
-        errors.iat = 'Required';
-      }
-      if (!values.jti) {
-        errors.jti = 'Required';
-      }
-      if (!values.client_name) {
-        errors.client_name = 'Required';
-      }
-      if (!values.contacts) {
-        errors.contacts = 'Required';
-      }
-      if (!values.grant_types) {
-        errors.grant_types = 'Required';
-      }
-      if (!values.token_endpoint_auth_method) {
-        errors.token_endpoint_auth_method = 'Required';
-      }
-      if (!values.scope) {
-        errors.scope = 'Required';
-      }
-      return errors;
-    },
-    onSubmit: async function(values, { setSubmitting }){
+  //     if (!values.iss) {
+  //       errors.iss = 'Required';
+  //     }
+  //     if (!values.sub) {
+  //       errors.sub = 'Required';
+  //     }
+  //     if (!values.aud) {
+  //       errors.aud = 'Required';
+  //     }
+  //     if (!values.exp) {
+  //       errors.exp = 'Required';
+  //     }
+  //     if (!values.iat) {
+  //       errors.iat = 'Required';
+  //     }
+  //     if (!values.jti) {
+  //       errors.jti = 'Required';
+  //     }
+  //     if (!values.client_name) {
+  //       errors.client_name = 'Required';
+  //     }
+  //     if (!values.contacts) {
+  //       errors.contacts = 'Required';
+  //     }
+  //     if (!values.grant_types) {
+  //       errors.grant_types = 'Required';
+  //     }
+  //     if (!values.token_endpoint_auth_method) {
+  //       errors.token_endpoint_auth_method = 'Required';
+  //     }
+  //     if (!values.scope) {
+  //       errors.scope = 'Required';
+  //     }
+  //     return errors;
+  //   },
+  //   onSubmit: async function(values, { setSubmitting }){
 
-      console.log('Trying to register UDAP client...')
-      // console.log('values', values);
+  //     console.log('Trying to register UDAP client...')
+  //     // console.log('values', values);
 
-      let jwtPayload = {
-        iss: get(values, 'iss'),
-        sub: get(values, 'sub'),
-        aud: get(values, 'aud'),
-        exp: parseInt(get(values, 'exp')),
-        iat: parseInt(get(values, 'iat')),
-        jti: get(values, 'jti'),
-        client_name: get(values, 'client_name'),
-        redirect_uris: get(values, 'redirect_uris'),
-        contacts: [get(values, 'contacts')],
-        logo_uri: get(values, 'logo_uri'),
-        grant_types: [get(values, 'grant_types')],
-        response_types: get(values, 'response_types'),
-        token_endpoint_auth_method: get(values, 'token_endpoint_auth_method'),
-        scope: get(values, 'scope')
-      }
+  //     let jwtPayload = {
+  //       iss: get(values, 'iss'),
+  //       sub: get(values, 'sub'),
+  //       aud: get(values, 'aud'),
+  //       exp: parseInt(get(values, 'exp')),
+  //       iat: parseInt(get(values, 'iat')),
+  //       jti: get(values, 'jti'),
+  //       client_name: get(values, 'client_name'),
+  //       redirect_uris: get(values, 'redirect_uris'),
+  //       contacts: [get(values, 'contacts')],
+  //       logo_uri: get(values, 'logo_uri'),
+  //       grant_types: [get(values, 'grant_types')],
+  //       response_types: get(values, 'response_types'),
+  //       token_endpoint_auth_method: get(values, 'token_endpoint_auth_method'),
+  //       scope: get(values, 'scope')
+  //     }
 
-      let jwtHeader = {
-        "alg": "RS256",
-        "x5c": []
-      };
+  //     let jwtHeader = {
+  //       "alg": "RS256",
+  //       "x5c": []
+  //     };
 
-      console.log('jwtHeader', jwtHeader)
-      console.log('jwtPayload', jwtPayload);
+  //     console.log('jwtHeader', jwtHeader)
+  //     console.log('jwtPayload', jwtPayload);
 
 
-      try {
-        // HMAC SHA256
-        // var hmacSha256Token = jwt.sign(jwtPayload, 'shhhhh');
-        // console.log('hmacSha256Token', hmacSha256Token);
+  //     // try {
+  //     //   // HMAC SHA256
+  //     //   // var hmacSha256Token = jwt.sign(jwtPayload, 'shhhhh');
+  //     //   // console.log('hmacSha256Token', hmacSha256Token);
 
-        // var rs256Token = jwt.sign(jwtPayload, jwtHeader.x5c, {algorithm: jwtHeader.alg});
-        // console.log('rs256Token', rs256Token);
+  //     //   // var rs256Token = jwt.sign(jwtPayload, jwtHeader.x5c, {algorithm: jwtHeader.alg});
+  //     //   // console.log('rs256Token', rs256Token);
 
-        Meteor.call('generateAndSignCertificate', jwtPayload, function(error, signResult){
-          if(error){
-            console.log('error', error);
-          }
-          if(signResult){
-            console.log('signResult', signResult);
+  //     //   // Meteor.call('generateAndSignCertificate', jwtPayload, function(error, signResult){
+  //     //   //   if(error){
+  //     //   //     console.log('error', error);
+  //     //   //   }
+  //     //   //   if(signResult){
+  //     //   //     console.log('signResult', signResult);
 
-            // if we have a certificate from the server, we can begin constructing the JWT header
-            // note:  may want to move this server side
-            if(get(signResult, 'token')){
-              let decoded = jwt.decode(get(signResult, 'token'));
+  //     //   //     // if we have a certificate from the server, we can begin constructing the JWT header
+  //     //   //     // note:  may want to move this server side
+  //     //   //     if(get(signResult, 'token')){
+  //     //   //       let decoded = jwt.decode(get(signResult, 'token'));
 
-              console.log('decoded', decoded)
-            }
+  //     //   //       console.log('decoded', decoded)
+  //     //   //     }
 
-            // now we can construct the JWT 
+  //     //   //     // now we can construct the JWT 
             
 
-          }
-        })
+  //     //   //   }
+  //     //   // })
   
 
-      } catch (err) {
-        console.log('err', err)
-      }
-    }
-  });
+  //     // } catch (err) {
+  //     //   console.log('err', err)
+  //     // }
+  //   }
+  // });
 
 
-  //----------------------------------------------------------------------
-  // Carousel  
+  // //----------------------------------------------------------------------
+  // // Carousel  
 
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-      paritialVisibilityGutter: 60
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-      paritialVisibilityGutter: 50
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-      paritialVisibilityGutter: 30
-    }
-  };
+  // const responsive = {
+  //   desktop: {
+  //     breakpoint: { max: 3000, min: 1024 },
+  //     items: 3,
+  //     paritialVisibilityGutter: 60
+  //   },
+  //   tablet: {
+  //     breakpoint: { max: 1024, min: 464 },
+  //     items: 2,
+  //     paritialVisibilityGutter: 50
+  //   },
+  //   mobile: {
+  //     breakpoint: { max: 464, min: 0 },
+  //     items: 1,
+  //     paritialVisibilityGutter: 30
+  //   }
+  // };
 
 
   function openExternalPage(url){
