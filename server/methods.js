637e8bcc884b89a4f201cf56808733fa9682d4a0
Abigail Watson <awatson@mitre.org>
2022-01-11 16:49:36 -0600

    sig methods

--- server/methods.js
+++ server/methods.js
@@ -67,79 +67,22 @@ Meteor.methods({
         console.log("Signing certificate...")
         console.log('jwtPayload', jwtPayload);
 
-        // let privateKeyPem = get(Meteor, 'settings.private.x509.privateKey');
-        // let publicKeyPem = get(Meteor, 'settings.private.x509.publicKey');
-
-        // let privateKey = pki.privateKeyFromPem(privateKeyPem)
-        // let publicKey = pki.publicKeyFromPem(publicKeyPem)
-
         let result = {};
 
-        // if(privateKey){
-        //     console.log('privateKey', privateKey)
-        //     console.log('publicKey', publicKey)
+        let privateKeyPem = get(Meteor, 'settings.private.x509.publicCertPem');
+        console.log('privateKeyPem', privateKeyPem)            
 
-        //     // var hmacSha256Token = jwt.sign(jwtPayload, 'shhhhh');
-        //     // console.log('hmacSha256Token', hmacSha256Token);
-
-        //     let cert = pki.createCertificate();
-
-        //     cert.publicKey = publicKey;
-        //     cert.serialNumber = '01';
-        //     cert.validity.notBefore = new Date();
-        //     // cert.validity.notAfter = new Date();
-        //     cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear() + 1);
-
-        //     var attrs = [{
-        //         name: 'commonName',
-        //         value: 'mitre.org'
-        //         }, {
-        //         name: 'countryName',
-        //         value: 'US'
-        //         }, {
-        //         shortName: 'ST',
-        //         value: 'Illinois'
-        //         }, {
-        //         name: 'localityName',
-        //         value: 'Chicago'
-        //         }, {
-        //         name: 'organizationName',
-        //         value: 'MITRE'
-        //         }, {
-        //         shortName: 'OU',
-        //         value: 'MITRE'
-        //     }];
-        //     // cert.setSubject(attrs);
-        //     cert.setIssuer(attrs);
-        //     cert.sign(privateKey);
-
-        //     console.log('cert', cert);
-
-        //     var certificatePem = pki.certificateToPem(cert);
-        //     console.log('certificatePem', certificatePem)
-
-        //     result.cert = cert;
-        //     result.certificatePem = certificatePem;
-        //     // var rs256Token = jwt.sign(jwtPayload, privateKey, {algorithm: "RS256"});
-        //     // console.log('rs256Token', rs256Token);
-
-
-            let privateKeyPem = get(Meteor, 'settings.private.x509.publicCertPem');
-            console.log('privateKeyPem', privateKeyPem)            
-
-            jwt.sign(jwtPayload, privateKeyPem.trim(), {
-                algorithm: 'RS256',
-            }, function(error, token){
-                if(error){
-                    console.log('error', error)
-                }
-                if(token){
-                    console.log('token', token)
-                    result.token = token;
-                }
-            });
-    
-        }
+        jwt.sign(jwtPayload, privateKeyPem.trim(), {
+            algorithm: 'RS256',
+        }, function(error, token){
+            if(error){
+                console.log('error', error)
+            }
+            if(token){
+                console.log('token', token)
+                result.token = token;
+            }
+        });
 
         return result;
     },
