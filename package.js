b8af5014494c69c2310838a20d5e8a2ec491a391
Abigail Watson <awatson@mitre.org>
2022-05-03 16:09:04 -0500

    bump deps

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mitre:national-directory',
-  version: '0.13.2',
+  version: '0.13.3',
   summary: 'Validated Care Coordination Directory',
   // git: 'http://github.com/mitre/vhdir-core',
   documentation: 'README.md'
